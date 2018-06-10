c9bdf1f50719f296427123827027566b83c178b1
Kail Keusch <kfkkail@gmail.com>
2018-06-10 10:02:41 -0400

    Ability to add Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,33 @@ class UsersController < ApplicationController
   def index
     @users = policy_scope(User)
   end
+
+  def new
+    @user = User.new
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @user = User.new(user_params)
+    @user.org_id = current_user.org_id
+    @user.password = 'kailkeusch.123'
+    @user.password_confirmation = 'kailkeusch.123'
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,8 @@ ActiveRecord::Schema.define(version: 20180610005426) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_
+password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
